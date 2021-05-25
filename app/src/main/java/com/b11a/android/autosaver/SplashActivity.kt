04f5ae2e9ce4d874f677618bd1683f47b56fb0fb
SubinJang <jsb408@gmail.com>
2021-05-25 13:02:16 +0900

    fix: Check Permission Error(Check permission in Splash Activity)

--- app/src/main/java/com/b11a/android/autosaver/SplashActivity.kt
+++ app/src/main/java/com/b11a/android/autosaver/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.b11a.android.autosaver
 
+import android.Manifest.permission.*
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.b11a.android.autosaver.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
+            checkPermission(ACCESS_FINE_LOCATION, SEND_SMS, CALL_PHONE)
             startActivity(
                 Intent(this,
                 if(kPrefs(this).getBoolean("autoLogin", false)
@@ -20,10 +23,15 @@ class SplashActivity : AppCompatActivity() {
                     MainActivity::class.java
                 } else {
                     LoginActivity::class.java
-                }
-            )
+                })
             )
             finish()
         }, 1500)
+    }
+
+    private fun checkPermission(vararg permissions: String) {
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it.containsValue(false)) finish()
+        }.launch(permissions)
     }
 }
